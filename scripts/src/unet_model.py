d0a8e97a8258a4a8c1175d2b260b702e5dfa93ee
SalimFares4 <fares01@ads.uni-passau.de>
2024-07-18 16:23:10 +0000

    Added dropout

--- scripts/src/unet_model.py
+++ scripts/src/unet_model.py
@@ -5,22 +5,22 @@ from unet_parts import *
 
 
 class UNet(nn.Module):
-    def __init__(self, n_channels, n_classes, bilinear=False):
+    def __init__(self, n_channels, n_classes, bilinear=False, dropout=None):
         super(UNet, self).__init__()
         self.n_channels = n_channels
         self.n_classes = n_classes
         self.bilinear = bilinear
 
-        self.inc = (DoubleConv(n_channels, 64))
-        self.down1 = (Down(64, 128))
-        self.down2 = (Down(128, 256))
-        self.down3 = (Down(256, 512))
+        self.inc = (DoubleConv(n_channels, out_channels=64))
+        self.down1 = (Down(in_channels=64, out_channels=128))
+        self.down2 = (Down(in_channels=128, out_channels=256))
+        self.down3 = (Down(in_channels=256, out_channels=512))
         factor = 2 if bilinear else 1
-        self.down4 = (Down(512, 1024 // factor))
-        self.up1 = (Up(1024, 512 // factor, bilinear))
-        self.up2 = (Up(512, 256 // factor, bilinear))
-        self.up3 = (Up(256, 128 // factor, bilinear))
-        self.up4 = (Up(128, 64, bilinear))
+        self.down4 = (Down(in_channels=512, out_channels = 1024 // factor, dropout=dropout))
+        self.up1 = (Up(in_channels=1024, out_channels =512 // factor, bilinear=bilinear))
+        self.up2 = (Up(in_channels=512, out_channels =256 // factor, bilinear=bilinear))
+        self.up3 = (Up(in_channels=256, out_channels =128 // factor, bilinear=bilinear))
+        self.up4 = (Up(in_channels=128, out_channels =64, bilinear=bilinear))
         self.outc = (OutConv(64, n_classes))
 
     def forward(self, x):
